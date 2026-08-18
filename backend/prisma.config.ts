3ef625bb60982f7b16b08565236b37a7e7c38b44
hariyanto17 <hariyanto17@mhs.akba.ac.id>
2026-08-18 17:16:21 +0800

    chore: setup prisma config and scripts for format, push, and pull

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from "@prisma/config";
-import env from "dotenv"
+import { defineConfig } from "prisma/config";
+import dotenv from "dotenv";
 
-env.config()
+dotenv.config();
 
 export default defineConfig({
+  schema: "prisma/schema.prisma",
   datasource: {
     url: process.env.DATABASE_URL || "",
   },
 });
+
